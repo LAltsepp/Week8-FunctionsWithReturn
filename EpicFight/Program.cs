a7ef96cbe484e0fe98e7f20815ebad8cf65c03ba
Opilane TTHK <opilane@tthk.ee>
2021-10-21 09:52:21 +0300

    first commit

---
+++ EpicFight/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EpicFight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hero, villain;
+            hero = RandomHero();
+            villain = RandomVillain();
+            Console.WriteLine($"Random hero: {hero}");
+            Console.WriteLine($"Random villain: {villain}");
+        }
+        private static string RandomHero()
+        {
+            string[] heroes = { "Batman", "Spongebob", "Spiderman", "Patric", "Lara Croft" };
+            Random rnd = new Random();
+            int RandomIndex = rnd.Next(0, heroes.Length);
+            string randomHero = heroes[RandomIndex];
+            return randomHero;
+            Console.WriteLine($"Computer chose random hero be {randomHero}");
+        }
+        private static string RandomVillain()
+        {
+            string[] villains = { "Emme", "Issi", "Vanaema" };
+            Random rnd = new Random();
+            int RandomIndex = rnd.Next(0, villains.Length);
+            string randomVillain = villains[RandomIndex];
+            return randomVillain;
+        }
+    }
+}
